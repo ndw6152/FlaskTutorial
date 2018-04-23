4831880a8f8ec256871f0a9b460643c4f0b8e769
ndw6152 <ndwong19@gmail.com>
2018-04-22 17:33:34 -0700

    Added new db and the migration scripts

--- helloapp/__init__.py
+++ helloapp/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-
 from helloapp.config import DebugConfig
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(DebugConfig)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-from helloapp import routes
+from helloapp import routes, models
--- helloapp/routes.py
+++ helloapp/routes.py
@@ -20,15 +20,15 @@ def home_index():
     return render_template('index.html', title='Flask microblog', user=user, posts=posts)
 
 
-@app.route('/test')
-def test2():
-    return "Hello, TEST!"
+@app.route('/test/<test>')
+def test2(test):
+    return "Hello, %s!" % test
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format( form.username.data, form.remember_me.data))
-        return redirect(url_for('home_index'))
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('home_index', username=form.username.data))
     return render_template('login.html', title='Sign In', form=form)
